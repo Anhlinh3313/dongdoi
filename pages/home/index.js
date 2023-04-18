2a43accedb0ee8204b2de475945e3347dfd74a16
Gin <kjssmealong@gmail.com>
2023-04-18 09:43:32 +0700

    upade home index

--- pages/home/index.js
+++ pages/home/index.js
@@ -33,22 +33,22 @@ function Home() {
     return (
         <>
             <div className="body">
-                <div className={styles.banner}>
+                <div className={styles["banner"]}>
                     <img src="./banner.png" alt="Banner" />
                 </div>
-                <div className={styles.banner_sider}>
-                    <div className={styles.banner_sider_connent}>
+                <div className={styles["banner_sider"]}>
+                    <div className={styles["banner_sider_connent"]}>
                         <BannerSlider />
                     </div>
                 </div>
-                <section className={styles.event} id="section-event">
-                    <div className={styles.event_text}>
+                <section className={styles["event"]} id="section-event">
+                    <div className={styles["event_text"]}>
                         <h1>TIN TỨC - SỰ KIỆN</h1>
                         <div>Hãy cập nhật những dự án và chương trình mới nhất của chúng tôi. Và đồng hành lan tỏa những điều thiện!</div>
-                        <div className={styles.event_line}></div>
+                        <div className={styles["event_line"]}></div>
                     </div>
 
-                    <div className={styles.event_slide}>
+                    <div className={styles["event_slide"]}>
                         <Slide />
                     </div>
 
