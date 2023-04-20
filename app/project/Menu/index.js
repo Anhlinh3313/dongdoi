da387478bcff1a496a086adad3955218c54c0b36
Gin <kjssmealong@gmail.com>
2023-04-20 11:08:55 +0700

    update code

--- app/project/Menu/index.js
+++ app/project/Menu/index.js
@@ -1,16 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { reState } from "@useState/index";
+import React from "react";
 import MenuPC from "./MenuPC";
-import MenuMobile from "./MenuMobile";
 
 const Menu = () => {
-  const { chromeWidth } = React.useContext(reState);
-  const [widthWindow, setWidthWindow] = useState();
-  useEffect(() => {
-    setWidthWindow(chromeWidth);
-  }, [chromeWidth]);
-
-  return <>{widthWindow > 1050 ? <MenuPC /> : <MenuMobile />} </>;
+  return <MenuPC />;
 };
 
 export default Menu;
